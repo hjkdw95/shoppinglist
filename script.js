dcbca9bd744dd64b117e4cad022458f659a71576
Casey <hjkdw9506@gmail.com>
2021-05-25 21:12:05 +0900

    자동 스크롤링 기능 및 입력시 엔터 기능 보완

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let content = document.querySelector("form")
 let listInput = document.querySelector("input")
 let sumbit = document.querySelector(".submit")
 let lists = document.querySelector("ul")
@@ -12,7 +11,91 @@ let listSave = [];
 let idNumbers = 1;
 
 
-// 4. 지우는 함수 발동
+// 1. input에서 얻어온 값을 list에 올려주는 함수로 보낸다
+function onAdd(event){
+    //event.preventDefault();
+    const currentValue = listInput.value;
+    if(currentValue === ""){
+        listInput.focus();
+        return;
+    }
+    listUp(currentValue);
+    listInput.value = "";
+    listInput.focus();
+    
+}
+
+// 2. list에다 input 내역을 보아게 한다.
+// li는 안에 text와 버튼으로 구성되어있다
+function listUp(text){
+    // 2. li 입력 - text 입력될 때마다 라인 업데이트해줘야되서 지역변수로 둠
+    const list = document.createElement("li");
+
+    //2-1. 내용 들어가는 구간
+    const span = document.createElement("span");
+    span.innerText = text;
+
+    list.append(span);
+    lists.append(list);
+
+    // 새로 추가된 아이템으로 스크롤링
+    list.scrollIntoView({block: "center"});
+
+
+
+    // 저장소에서 구분할 수 있게 id지정
+    const newId = idNumbers;
+    idNumbers += 1;
+    list.id = newId;
+    // 내용을 저장소로 쓸 array에 넣어주기
+    const listObj = {
+        text: text,
+        id: newId
+    };
+    listSave.push(listObj);
+    // 내용 안날라가게 업데이트 한 array를 local storage에 저장시키기
+    saveLists();
+
+
+
+    //2-2. delete버튼 생성
+    const delBtn = document.createElement("button");
+    delBtn.setAttribute('class', 'trash');
+    delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`
+    list.append(delBtn);
+
+
+    // 아이콘은 변하지 않으니까 innerHTML로 처리
+    //const delIcon = document.createElement("i");
+    //delIcon.setAttribute('class', 'fas fa-trash-alt');
+    //delBtn.append(delIcon);
+    
+
+    //지우는 함수 발동
+    delBtn.addEventListener("click", deleteItem);
+}
+
+
+// 3. 내용 안날라가게 업데이트 한 array를 local storage에 저장시키기
+function saveLists(){
+    localStorage.setItem(list_LS, JSON.stringify(listSave))
+}
+
+// 3-1. LS에 저장된 데이터를 화면에 보이게 할 함수
+function loadLists(){
+    //이미 저장된 리스트
+    const loadedLists = localStorage.getItem(list_LS);
+    // list가 비어있지 않다면 string인 array를 다시 풀어서 활용할 수 있게 하기
+    if(loadedLists !== null){
+        const parsedLists = JSON.parse(loadedLists);
+        //array로 들어온 것을 각각 list에 showup하기
+        parsedLists.forEach(list => {
+            listUp(list.text)
+        });
+    }
+}
+
+// 4. 지우는 함수
 function deleteItem(event){
     // ls 내부 데이터 삭제과정
     
@@ -33,89 +116,17 @@ function deleteItem(event){
 }
 
 
-// 3. 내용 안날라가게 업데이트 한 array를 local storage에 저장시키기
-function saveLists(){
-    localStorage.setItem(list_LS, JSON.stringify(listSave))
-}
-
-// 2. list에다 input 내역을 보아게 한다.
-// li는 안에 text와 버튼으로 구성되어있다
-function listUp(text){
-    // 2. li 입력 - text 입력될 때마다 라인 업데이트해줘야되서 지역변수로 둠
-    const list = document.createElement("li");
-
-    //2-1. 내용 들어가는 구간
-    const span = document.createElement("span");
-    span.innerText = text;
-
-    list.append(span);
-    lists.append(list);
-
-    // 저장소에서 구분할 수 있게 id지정
-    const newId = idNumbers;
-    idNumbers += 1;
-    list.id = newId;
-    // 내용을 저장소로 쓸 array에 넣어주기
-    const listObj = {
-        text: text,
-        id: newId
-    };
-    listSave.push(listObj);
-    // 내용 안날라가게 업데이트 한 array를 local storage에 저장시키기
-    saveLists();
-
-    //2-2. delete버튼 생성
-    const delBtn = document.createElement("button");
-    delBtn.setAttribute('class', 'trash');
-    delBtn.innerHTML = `<i class="fas fa-trash-alt"></i>`
-    list.append(delBtn);
-
-
-    // 아이콘은 변하지 않으니까 innerHTML로 처리
-    //const delIcon = document.createElement("i");
-    //delIcon.setAttribute('class', 'fas fa-trash-alt');
-    //delBtn.append(delIcon);
-    
-
-    //지우는 함수 발동
-    delBtn.addEventListener("click", deleteItem);
-}
-
-// 1. input에서 얻어온 값을 list에 올려주는 함수로 보낸다
-function onAdd(event){
-    event.preventDefault();
-    const currentValue = listInput.value;
-    if(currentValue === ""){
-        listInput.focus();
-        return;
-    }
-    listUp(currentValue);
-    listInput.value = "";
-    listInput.focus();
-    
-}
-
-// 3. LS에 저장된 데이터를 화면에 보이게 할 함수
-function loadLists(){
-    //이미 저장된 리스트
-    const loadedLists = localStorage.getItem(list_LS);
-    // list가 비어있지 않다면 string인 array를 다시 풀어서 활용할 수 있게 하기
-    if(loadedLists !== null){
-        const parsedLists = JSON.parse(loadedLists);
-        //array로 들어온 것을 각각 list에 showup하기
-        parsedLists.forEach(list => {
-            listUp(list.text)
-        });
-    }
-}
-
 
 
 
 function init(){
     loadLists();
     sumbit.addEventListener("click", onAdd);
-    content.addEventListener("submit",onAdd);
+    listInput.addEventListener("keypress", (event) => {
+        if(event.key === "Enter"){
+            onAdd();
+        }
+    });
 }
 
 init();
